b0cc604e7c16c8c068dc16387385e68f1e6ac0a7
bgbro <bgbro.inc@gmail.com>
2025-04-29 13:42:01 +0300

    completed

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -127,6 +127,9 @@ USE_I18N = True
 
 USE_TZ = False
 
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
